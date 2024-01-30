3ce27c128856345757ed01f4c40235b4db048a89
yejin0901 <61917664+yejin0901@users.noreply.github.com>
2024-01-30 11:46:28 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.67 ms, Memory: 9.93 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    result = set()
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            result.add(numbers[i]+numbers[j])
+            
+    return sorted(list(result))
+    
