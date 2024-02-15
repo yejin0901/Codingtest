de4c3ec5e0660d3b5bbd2d1a475041fd2015bd6f
yejin0901 <61917664+yejin0901@users.noreply.github.com>
2024-02-15 19:50:42 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,11 @@
+def solution(X, Y):    
+    xy = set(X) & set(Y)
+    if not xy:
+        return "-1"
+    
+    elif len(xy) ==1 and "0"in xy:
+        return "0"
+
+    result = [n * min(X.count(n), Y.count(n)) for n in xy]
+
+    return "".join(sorted(result, reverse=True))
