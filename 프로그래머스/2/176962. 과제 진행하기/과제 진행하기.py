befef032721a9a5af6c97bdbb7182ff42a166a51
yejin0901 <61917664+yejin0901@users.noreply.github.com>
2024-05-16 00:12:54 +0900

    [level 2] Title: 과제 진행하기, Time: 2.88 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -8,33 +8,37 @@ def convert_to_number(time): #문자열
 
 def solution(plans):
     p = []
+    answer = []
     
     for title,start,time in plans:
         p.append((title, convert_to_number(start), convert_to_number(time)))
     
     p.sort(key=lambda x:x[1])
-    ans = []
-    stack = []
-    for i in range(len(p)): # 시간안에 과제를 못 끝낼경우, 끝낼경우
-        if i == len(p)-1: # 마지막 과제에 도달했을 경우
-            ans.append(p[i][0])
-            for i in range(-1, -len(stack)-1, -1):
-                ans.append(stack[i][0])
-            break
-        extra = p[i+1][1] - (p[i][1]+p[i][2]) # 다음과제시간 - (현재 과제시간 + 과제 진행시간)
-        
-        if extra >= 0: # 시간 안에 끝낼수 있는경우 추가과제 할수 있으면 한다
-            ans.append(p[i][0])
-            while stack:
-                if stack[-1][1] <= extra: # 여분의 시간이 스택에 남은 시간보다 클 경우
-                    k = stack.pop()
-                    ans.append(k[0]) # 과제이름
-                    extra -= k[1]
-                
-                else:
-                    stack[-1][1] -= extra
-                    break
-        else: # 다음 과제시간까지 못 끝낸경우
-            stack.append([p[i][0], -extra])
     
-    return ans
+    stack = []
+    stack.append(p[0])
+    
+    time = p[0][1]
+    
+    for i in range(1,len(plans)):
+        next_time = p[i][1]
+        
+        while stack:
+            job, time_start, time_spend = stack.pop()
+            
+            if time < time_start:
+                time = time_start
+            time_finish = time + time_spend
+            
+            if next_time < time_finish:
+                stack.append([job, time_start, time_finish - next_time])
+                time = next_time
+                break
+            else:
+                answer.append(job)
+                time += time_spend
+                
+        stack.append(p[i])
+    while len(stack):
+        answer.append(stack.pop()[0])
+    return answer 
